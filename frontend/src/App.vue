34a8f0147e5bdaba11b748ba5bef44b7aa014520
loyiv <165025787+loyiv@users.noreply.github.com>
2025-12-24 18:59:43 +0800

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterView />
+  <AppToast />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import AppToast from './components/AppToast.vue'
+</script>
+
+<style scoped>
+</style>
+
+
